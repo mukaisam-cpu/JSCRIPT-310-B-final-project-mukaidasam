97710795ca8f1bdbd17ca4b36783b66925a6e593
mukaisam-cpu <mukaisam@uw.edu>
2025-12-05 18:25:02 -0800

    Tests for gameList

---
+++ tests/gameList.spec.js
@@ -0,0 +1,15 @@
+describe("Saved Games List", () => {
+    it("Should instantiate with an empty list if there is nothing saved in local storage", 
+        () => {
+
+        }
+    );
+
+    it("Should automatically load the list from storage if one is present", () => {
+
+    });
+
+    it("Should delete both the list and clear the saved data in local storage", () => {
+
+    });
+});
