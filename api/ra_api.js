8f629d762960491937b4b143d01115b0bd05efb2
mukaisam-cpu <mukaisam@uw.edu>
2025-12-06 16:16:27 -0800

    ra_api get games for system

--- api/ra_api.js
+++ api/ra_api.js
@@ -1,10 +1,35 @@
+/**
+ * Functions for RetroAchievements API calls
+ * @param {number} delay Delay 
+ */
 class RA_API {
+    constructor(delay) {
+        this.delay = delay;
+    }
+
     /**
      * A call to this endpoint will retrieve the complete list of all system ID and name 
-     * pairs on the site. 
+     * pairs on the site.
+     * 
+     * Returning only active systems with achievements and excluding hubs. Query parameters
+     * 'a' and 'g' are set to 1.
      * */
     async getAllSystems() {
         const url = `https://retroachievements.org/API/API_GetConsoleIDs.php?&y=${API_KEY}&a=1&g=1`
+        return(fetch(url)
+            .then(response => response.json()));
+    }
+
+    /**
+     * A call to this endpoint will retrieve the complete list of games for a specified 
+     * console on the site, targeted by the console ID. If you do not know the console ID 
+     * you're looking for, try using the all systems endpoint.
+     * 
+     * Returning only games with achievements. Query parameter 'f' is set to 1.
+     * @param {string} systemID 
+     */
+    async getGamesForSystem(systemID) {
+        const url = `https://retroachievements.org/API/API_GetGameList.php?&y=${API_KEY}&i=${systemID}&f=1`
         return(fetch(url)
             .then(response => response.json()));
     }
--- tests/ra_api.spec.js
+++ tests/ra_api.spec.js
@@ -1,9 +1,20 @@
 describe("RetroAchievements API", () =>{
-    it("should get all active non-hub systems", async () => {
+    it("Should get all active non-hub systems", async () => {
         api = new RA_API();
         const result = await api.getAllSystems();
         console.log(result);
         expect(result).toEqual(jasmine.anything());
         expect(result.length).toEqual(53);
-    })
+    });
+
+    it("Should return all games with achievement sets for the specified system ID", 
+        async () => {
+            api = new RA_API();
+            testID = "41";
+            const result = await api.getGamesForSystem(testID);
+            console.log(result);
+            debugger;
+
+            const testGame = 
+    });
 })
